c23359804377f2ed2ffb480f00793c8691610d57
Pallaviburoju <pallaviburoju@gmail.com>
2019-03-25 23:30:35 +0530

    static wepage with angular

---
+++ src/src/app/mobile-component/mobile-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-component',
+  templateUrl: './mobile-component.component.html',
+  styleUrls: ['./mobile-component.component.css']
+})
+export class MobileComponentComponent  {
+
+  
+
+}
